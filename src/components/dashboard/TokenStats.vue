9ae222b97e446494265eff2f49e0677d574cd23d
Chris Mitchell <cbm3384@gmail.com>
2018-06-16 23:20:22 -0600

    wip

--- src/components/dashboard/TokenStats.vue
+++ src/components/dashboard/TokenStats.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="dashboard--token-stats" :class="[{'show-receive': showReceive}]">
+  <section id="dashboard--token-stats" :class="[{'show-receive': showReceive, 'show-send': showSend}]">
     <div class="header">
       <div class="title">Token Stats</div>
       <div class="btn-group">
@@ -7,7 +7,7 @@
           <aph-icon name="receive"></aph-icon>
           <p>Receive</p>
         </div>
-        <div class="send-btn">
+        <div class="send-btn" @click="showSend = true">
           <aph-icon name="send"></aph-icon>
           <p>Send</p>
         </div>
@@ -109,6 +109,74 @@
         </div>
       </div>
     </div>
+    <template v-if="showSendConfirmation">
+      <div class="send confirmation">
+        <div class="control" @click="showSendConfirmation = false">
+          <aph-icon name="arrow-left"></aph-icon>
+          <div class="title">Send</div>
+        </div>
+        <div class="body">
+          <div class="inner">
+            <p>You are sending</p>
+            <aph-token-icon :symbol="$store.state.statsToken.symbol"></aph-token-icon>
+            <div class="amount">{{amount}} {{ $store.state.statsToken.symbol }}</div>
+            <div class="row">
+              <div class="col">
+                <div class="label">Value</div>
+                <div class="value">{{ $formatMoney($store.state.statsToken.unitValue * amount) }}</div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <div class="label">Token</div>
+                <div class="value">{{ $store.state.statsToken.name }}</div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <div class="label">Recipient</div>
+                <div class="value">{{ address }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button class="send-btn">Send</button>
+      </div>
+    </template>
+    <template v-else>
+      <div class="send" v-touch:swipe.down="hideSend">
+        <div class="control" @click="hideSend">
+          <aph-icon name="arrow-down"></aph-icon>
+          <div class="title">Send</div>
+        </div>
+        <div class="header">
+          <aph-token-icon :symbol="$store.state.statsToken.symbol"></aph-token-icon>
+          <div class="meta">
+            <div class="symbol">{{ $store.state.statsToken.symbol }}</div>
+            <div class="balance">{{ $formatNumber($store.state.statsToken.balance) }}</div>
+          </div>
+          <aph-icon name="arrow-right"></aph-icon>
+        </div>
+        <div class="body">
+          <div class="underlined">Send To</div>
+          <div class="tile send-to">
+            <aph-input placeholder="Address" :light="true" v-model="address"></aph-input>
+            <div class="help-text">
+              <div class="symbol">{{ $store.state.statsToken.symbol }}</div>
+            </div>
+          </div>
+          <div class="underlined">Amount</div>
+          <div class="tile amount">
+            <aph-input placeholder="Amount" :light="true" type="number" v-model="amount"></aph-input>
+            <div class="help-text">
+              <div class="value">{{ $formatMoney($store.state.statsToken.unitValue * amount) }}</div>
+              <div class="max" @click="amount = `${$store.state.statsToken.balance}`">Max</div>
+            </div>
+          </div>
+        </div>
+        <button class="next-btn" :disabled="shouldDisableNextButton" @click="showSendConfirmation = true">Next</button>
+      </div>
+    </template>
     <aph-transaction-detail :on-hide="hideTransactionDetail" :show="showTransactionDetail" :transaction="transactionDetail"></aph-transaction-detail>
     <aph-full-token-stats :on-hide="hideFullTokenStats" :show="showFullTokenStats" :token="$store.state.statsToken"></aph-full-token-stats>
   </section>
@@ -144,16 +212,24 @@ export default {
         return value < 0 && this.symbol === symbol;
       });
     },
+
+    shouldDisableNextButton() {
+      return !this.address || !this.amount || !parseFloat(this.amount);
+    },
   },
 
   data() {
     return {
       activeTile: 'preview',
       activeTransactionHistoryTab: 'sent',
+      address: '',
+      amount: '',
       high: 0,
       low: 0,
       showFullTokenStats: false,
       showReceive: false,
+      showSend: false,
+      showSendConfirmation: false,
       showTransactionDetail: false,
       transactionDetail: {},
       volume: 0,
@@ -189,6 +265,10 @@ export default {
 
     hideReceive() {
       this.showReceive = false;
+    },
+
+    hideSend() {
+      this.showSend = false;
     },
 
     hideTransactionDetail() {
@@ -539,6 +619,7 @@ export default {
       }
 
       .title {
+        font-size: toRem(18px);
         text-align: center;
       }
     }
@@ -577,14 +658,216 @@ export default {
         }
       }
     }
+  }
 
-    .submit-btn {
+  > .send {
+    @include transition(top);
+
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    left: 0;
+    overflow: hidden;
+    position: fixed;
+    top: 100vh;
+    width: 100%;
+    z-index: 200;
+
+    > .control {
+      background: $dark;
+      padding: $space;
+      flex: none;
+      position: relative;
+
+      .aph-icon {
+        position: absolute;
+
+        svg {
+          height: toRem(20px);
+        }
+      }
+
+      .title {
+        font-size: toRem(18px);
+        text-align: center;
+      }
+    }
+
+    > .header {
+      background: white;
+      border-radius: $border-radius;
+      box-shadow: $box-shadow;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      height: toRem(70px);
+      padding: 0 $space;
+      color: $dark;
+      margin: 0 $space toRem(-35px) $space;
+      z-index: 100;
+
+      .aph-token-icon {
+        flex: none;
+      }
+
+      .meta {
+        flex: 1;
+        margin-left: $space;
+        font-size: toRem(18px);
+
+        .symbol {
+          font-family: GilroyMedium;
+        }
+
+        .balance {
+          @extend %small-uppercase-grey-label-dark;
+
+          font-size: toRem(18px);
+        }
+      }
+
+      .aph-icon {
+        flex: none;
+
+        svg {
+          height: toRem(18px);
+        }
+
+        .fill {
+          fill: $dark;
+        }
+      }
+    }
+
+    > .body {
+      @extend %tile-grid;
+
+      background: $background;
+      border-top-left-radius: $border-radius;
+      border-top-right-radius: $border-radius;
+      color: $dark;
+      flex: 1;
+      overflow: auto;
+      display: flex;
+      flex-direction: column;
+      padding: $space;
+
+      > .inner {
+        background: white;
+        border-radius: $border-radius;
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        overflow: hidden;
+        padding: $space;
+      }
+
+      .underlined {
+        color: $dark;
+        padding-bottom: $space;
+        margin: $space 0;
+        position: relative;
+        font-family: GilroyMedium;
+        margin-left: $space;
+
+        &:after {
+          content: "";
+          height: $border-width;
+          background: $purple;
+          width: toRem(30px);
+          position: absolute;
+          left: 0;
+          bottom: 0;
+        }
+
+        &:first-child {
+          margin-top: toRem(35px);
+        }
+      }
+
+      .tile {
+        background: white;
+        border-radius: $border-radius;
+        padding: $space;
+
+        & + .underlined {
+          margin-top: $space-lg;
+        }
+
+        &.send-to {
+          .help-text {
+            margin-top: $space;
+
+            .symbol {
+              @extend %small-uppercase-grey-label;
+
+              text-align: right;
+            }
+          }
+        }
+
+        &.amount {
+          .help-text {
+            display: flex;
+            flex-direction: row;
+            margin-top: $space;
+
+            .value {
+              @extend %small-uppercase-grey-label;
+
+              flex: 1;
+            }
+
+            .max {
+              @extend %small-uppercase-grey-label;
+
+              color: $purple;
+              flex: none;
+              text-align: right;
+            }
+          }
+        }
+      }
+    }
+
+    .next-btn, .send-btn {
       @extend %btn-footer;
+    }
+
+    &.confirmation {
+      p {
+        margin: 0;
+        text-align: center;
+      }
+
+      .aph-token-icon {
+        text-align: center;
+        margin: $space-lg 0 0;
+
+        img {
+          height: auto;
+          width: toRem(125px);
+        }
+      }
+
+      .amount {
+        @extend %small-uppercase-grey-label-dark;
+
+        font-size: toRem(20px);
+        margin: $space-lg 0;
+        text-align: center;
+      }
     }
   }
 
   &.show-receive {
     .receive {
+      top: 0;
+    }
+  }
+
+  &.show-send {
+    .send {
       top: 0;
     }
   }
