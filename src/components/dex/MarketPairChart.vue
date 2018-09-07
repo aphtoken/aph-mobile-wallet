fdbb2fb9161ede75a64e23cbc6c5118e0b8804a0
schaeferCode <scottschaef@gmail.com>
2018-09-06 20:58:33 -0400

    style chart component

--- src/components/dex/MarketPairChart.vue
+++ src/components/dex/MarketPairChart.vue
@@ -5,39 +5,43 @@
       {{ $store.state.currentMarket ? $store.state.currentMarket.marketName : '' }}
     </div>
     <div class="body">
-      <div class="day-values">
+      <div class="chart-header">
         <div class="add-button">
           Heart/Add
         </div>
-        <div class="row">
-          <div class="label">VOL.</div>
-          <div class="value">{{ $formatNumber($store.state.tradeHistory ? $store.state.tradeHistory.volume24Hour : 0) }}</div>
-        </div>
-        <div class="row">
-          <div class="label">OPEN</div>
-          <div class="value">{{ $formatNumber($store.state.tradeHistory ? $store.state.tradeHistory.open24Hour : 0) }}</div>
-        </div>
-        <div class="row">
-          <div class="label">HIGH</div>
-          <div class="value">{{ $formatNumber($store.state.tradeHistory ? $store.state.tradeHistory.high24Hour : 0) }}</div>
-        </div>
-        <div class="row">
-          <div class="label">LOW</div>
-          <div class="value">{{ $formatNumber($store.state.tradeHistory ? $store.state.tradeHistory.low24Hour : 0) }}</div>
-        </div>
+        <aph-token-icon class="icon" v-if="$store.state.currentMarket && $store.state.currentMarket.quoteCurrency" :symbol="$store.state.currentMarket.quoteCurrency"></aph-token-icon>
       </div>
-      <div class="token-details">
-        <aph-token-icon v-if="$store.state.currentMarket && $store.state.currentMarket.quoteCurrency" :symbol="$store.state.currentMarket.quoteCurrency"></aph-token-icon>
-        <div class="base-price">
-          {{ $formatTokenAmount($store.state.tradeHistory ? $store.state.tradeHistory.close24Hour : 0) }}
+      <div class="chart">
+        <div class="day-values">
+          <div class="row">
+            <div class="label">VOL.</div>
+            <div class="value">{{ $formatNumber($store.state.tradeHistory ? $store.state.tradeHistory.volume24Hour : 0) }}</div>
+          </div>
+          <div class="row">
+            <div class="label">OPEN</div>
+            <div class="value">{{ $formatNumber($store.state.tradeHistory ? $store.state.tradeHistory.open24Hour : 0) }}</div>
+          </div>
+          <div class="row">
+            <div class="label">HIGH</div>
+            <div class="value">{{ $formatNumber($store.state.tradeHistory ? $store.state.tradeHistory.high24Hour : 0) }}</div>
+          </div>
+          <div class="row">
+            <div class="label">LOW</div>
+            <div class="value">{{ $formatNumber($store.state.tradeHistory ? $store.state.tradeHistory.low24Hour : 0) }}</div>
+          </div>
         </div>
-        <div class="base-price-converted">
-          $11.41
-        </div>
-        <div class="label">24H CHANGE ({{ $store.state.currentMarket ? $store.state.currentMarket.quoteCurrency : '' }})</div>
-        <div :class="['change', {decrease: $store.state.tradeHistory ? $store.state.tradeHistory.change24Hour < 0 : false, increase: $store.state.tradeHistory ? $store.state.tradeHistory.change24Hour > 0 : false}]">
-          {{ $formatNumber($store.state.tradeHistory ? $store.state.tradeHistory.change24Hour : 0) }}
-          ({{ $formatNumber(percentChangeAbsolute) }}%)
+        <div class="token-details">
+          <div class="base-price">
+            {{ $formatTokenAmount($store.state.tradeHistory ? $store.state.tradeHistory.close24Hour : 0) }}
+          </div>
+          <div class="base-price-converted">
+            $11.41
+          </div>
+          <div class="label">24H CHANGE ({{ $store.state.currentMarket ? $store.state.currentMarket.quoteCurrency : '' }})</div>
+          <div :class="['change', 'increase', {decrease: $store.state.tradeHistory ? $store.state.tradeHistory.change24Hour < 0 : false, increase: $store.state.tradeHistory ? $store.state.tradeHistory.change24Hour > 0 : false}]">
+            {{ $formatNumber($store.state.tradeHistory ? $store.state.tradeHistory.change24Hour : 0) }}
+            ({{ $formatNumber(percentChangeAbsolute) }}%)
+          </div>
         </div>
       </div>
     </div>
@@ -71,20 +75,99 @@ export default {
 #dex--market-pair-chart {
   display: flex;
   flex: 1;
-  flex-direction: row;
-  height: 100%;
-  overflow: hidden;
+  flex-direction: column;
+  background: $dark-purple;
+  margin: $space;
+  max-height: toRem(180px); // Can probably remove this once other components are added
 
-   > .body {
-    background: $dark-purple*1.25;
+  > .header {
+    height: $space;
+    padding: $space;
+  }
+
+  > .body {
     display: flex;
     flex-direction: column;
     flex: 1;
-    overflow: hidden;
+    justify-content: space-between;
+    padding: 0 $space $space;
 
-    > div {
-      margin: $space;
-      background: $dark-purple;
+    .chart-header {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .chart {
+      display: flex;
+      flex-direction: row;
+
+      .day-values {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        justify-content: flex-end;
+
+        .row {
+          display: flex;
+          margin-top: $space-sm;
+
+          .label {
+            @extend %small-uppercase-grey-label-dark;
+
+            flex: 1;
+          }
+
+          .value {
+            font-family: GilroySemibold;
+            font-size: toRem(12px);
+            flex: 2;
+          }
+        }
+      }
+
+      .token-details {
+        display: flex;
+        flex-direction: column;
+        flex: none;
+        justify-content: flex-end;
+
+        > div {
+          margin-top: $space-sm;
+          text-align: right;
+        }
+
+        .label {
+          @extend %small-uppercase-grey-label-dark;
+        }
+
+        .base-price {
+          font-family: GilroySemibold;
+          font-size: toRem(16px);
+        }
+
+        .base-price-converted {
+          font-family: GilroySemibold;
+          font-size: toRem(12px);
+        }
+
+        .change {
+          font-size: toRem(12px);
+
+          &.increase {
+            color: $green;
+
+            &:before {
+              content: "+";
+            }
+          }
+
+          &.decrease {
+            color: $red;
+          }
+        }
+      }
     }
   }
 }
