95f7cd58d818d5d4c2a379b5407748852e3ce58c
schaeferCode <scottschaef@gmail.com>
2018-09-29 10:00:43 -0400

    add trade history component

---
+++ src/components/dex/TradeHistory.vue
@@ -0,0 +1,40 @@
+<template>
+  <section id="dex--trade-history">
+    HELLO
+  </section>
+</template>
+
+<script>
+
+export default {
+
+  computed: {
+  },
+
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+
+  mounted() {
+  },
+
+  watch: {
+  },
+};
+
+</script>
+
+<style lang="scss">
+#dex--trade-history {
+  background: $dark-purple*1.25;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+
+}
+</style>
